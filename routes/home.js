9d4f982a0d8dfb57a74942a7bfbae257ff69d762
George Crabtree <supercrabtree@googlemail.com>
2016-09-25 19:14:14 +0200

    Add links to channels on homepage

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,27 @@
 'use strict'
 
 const html = require('bel')
+const firebase = require('firebase')
 
 module.exports = function home(params) {
-  return html`<h2>HOME</h2>`
+
+  firebase.database().ref('channels')
+    .on('value', snapshot => {
+      const channelList = document.querySelector('.channelList')
+
+      Object.keys(snapshot.val()).forEach(channel => {
+        channelList.appendChild(html`
+          <li><a href="/${channel}">${channel}</a></li>
+        `);
+      });
+
+    })
+
+
+  return html`
+    <div>
+      <h2>Channels</h2>
+      <ul class="channelList"></ul>
+    </div>
+  `
 }
