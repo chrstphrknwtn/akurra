793ff9d6bf02112f823dbfe8bdf1201f0ccce497
George Crabtree <supercrabtree@googlemail.com>
2016-09-25 13:51:39 +0200

    Refactor channel into it's own factory module

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const createChannel = require('./lib/create-channel')
 require('whatwg-fetch')
 const firebase = require('firebase/app')
 require('firebase/database')
@@ -7,23 +8,8 @@ const config = require('./config.json')
 firebase.initializeApp(config.firebase)
 
 const database = firebase.database()
+const channel = createChannel('banana', database)
 
-const channel = {
-  create: (channelName) => {
-    database.ref(`channels/${channelName}`).set({
-      playhead: 0
-    })
-  },
-  delete: (channelName) => {
-    database.ref(`channels/${channelName}`).remove()
-  },
-  addTrack: (channelName, track) => {
-    database.ref(`channels/${channelName}/playlist/${track.id}`).set(track)
-  }
-}
-
-fetch(`http://api.soundcloud.com/tracks/235287251?client_id=${config.soundcloud.client_id}`)
+fetch(`http://api.soundcloud.com/tracks/235287241?client_id=${config.soundcloud.client_id}`)
 .then(res => res.json())
-.then(track => {
-  channel.addTrack('banana', track)
-})
+.then(channel.addTrack)
